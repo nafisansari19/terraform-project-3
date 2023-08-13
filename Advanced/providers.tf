2d6648fb2daf297a25f001f850d61aadf5f4ba44
Nafis Ansari <ansari@Nafiss-MacBook-Pro.local>
2023-08-13 18:57:38 -0400

    version update

--- Advanced/providers.tf
+++ Advanced/providers.tf
@@ -2,10 +2,11 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.0"
+      version = "~> 5.0"
     }
   }
 }
+
 
 # Configure the AWS Provider
 provider "aws" {
